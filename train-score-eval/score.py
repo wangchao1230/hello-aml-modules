e04fa12d06c785b5eea7fb9204e376add2372f38
Jie Tong <jietong@microsoft.com>
2020-04-15 17:35:48 +0800

    Pipeline sdk sample

---
+++ train-score-eval/score.py
@@ -0,0 +1,14 @@
+import argparse
+
+parser = argparse.ArgumentParser("score")
+parser.add_argument("--model_input", type=str, help="Path of input model")
+parser.add_argument("--test_data", type=str, help="Path to test data")
+parser.add_argument("--score_output", type=str, help="Path of scoring output")
+
+args = parser.parse_args()
+
+lines = [f'Model path: {args.model_input}', f'Test data path: {args.test_data}', f'Scoring output path: {args.score_output}']
+with open(args.score_output, 'w') as file:
+    for line in lines:
+        print(line)
+        file.write(line + "\n")
