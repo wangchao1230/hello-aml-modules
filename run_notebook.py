adfbbd1cea6d88c9adc579fbd1a9c36b1746e2f5
liyw <liyw@microsoft.com>
2020-09-06 20:18:19 +0800

    change workspace config

--- run_notebook.py
+++ run_notebook.py
@@ -4,24 +4,27 @@ import os
 import sys
 import json
 import tempfile
+import ast
+import astunparse
 
 import papermill
 
 extension = '.ipynb'
 
 
-exps = ["subcription_id = '<your subscription ID>'",
-        "SUBSCRIPTION_ID = '<your subscription ID>'",
-        "WORKSPACE_NAME = '<your workspace name>'",
-        "workspace_name = '<your workspace name>'",
-        "RESOURCE_GROUP_NAME = '<your resource group>'",
-        "resource_group_name = '<your resource group>'",
+exps = ["your subscription ID",
+        "your workspace name",
+        "your resource group",
         "!az login -o none",
         "!az account set -s $SUBSCRIPTION_ID",
         "!az ml folder attach -w $WORKSPACE_NAME -g $RESOURCE_GROUP_NAME"
         ]
-# ws = "ws = Workspace.get(subscription_id='4faaaf21-663f-4391-96fd-47197c630979', resource_group='DesignerTestRG', name='DesignerTest-WCUS')\n"
-# workspace = "workspace = Workspace.get(subscription_id='4faaaf21-663f-4391-96fd-47197c630979', resource_group='DesignerTestRG', name='DesignerTest-WCUS')\n"
+
+ws_get = ast.parse("ws = Workspace.get(subscription_id='4faaaf21-663f-4391-96fd-47197c630979', resource_group='DesignerTestRG', name='DesignerTest-WCUS')\n").body[0]
+workspace_get = ast.parse("workspace = Workspace.get(subscription_id='4faaaf21-663f-4391-96fd-47197c630979', resource_group='DesignerTestRG', name='DesignerTest-WCUS')\n").body[0]
+
+ws_config = ast.parse("ws = Workspace.from_config()\n").body[0]
+workspace_config = ast.parse("workspace = Workspace.from_config()\n").body[0]
 
 
 def run_notebook(notebook, output_folder):
@@ -31,6 +34,7 @@ def run_notebook(notebook, output_folder):
         log_output=True,
         # engine_name="azureml_engine",
     )
+
 
 def replace_kernel_to_notebook(notebook_data):
     """
@@ -44,19 +48,61 @@ def replace_kernel_to_notebook(notebook_data):
         name="python3"
     )
 
-def replace_workspace(notebook_data, path):
-    cells = notebook_data["cells"]
-    for cell in cells:
-        if cell["cell_type"] == "code":
-            codes = cell["source"]
-            for i in range(len(codes)):
-                for exp in exps:
-                    if exp in codes[i]:
-                        codes[i] = ''
-                    if "ws = Workspace.from_config()" in codes[i]:
-                        codes[i] = "ws = Workspace.from_config(r'{}')\n".format(path)
-                    if "workspace = Workspace.from_config()" in codes[i]:
-                        codes[i] = "workspace = Workspace.from_config(r'{}')\n".format(path)
+
+def parse_ast(code_cell):
+    """
+    Parse the code ast obj from cell source code.
+
+    :param code_cell: the code cell to be parsed.
+    :type: dict
+
+    :return: The ast.
+    :rtype: ast obj
+    """
+    try:
+        source = ''.join([line for line in code_cell['source']])
+        cell_ast = ast.parse(source)
+    except Exception as e:
+        print("Error occurs while trying to ast parse code in notebook")
+        print(e)
+        raise
+    return cell_ast
+
+
+def replace_workspace(notebook_data):
+    cells = notebook_data.get('cells')
+
+    for cell_itr in range(len(cells)):
+        cell = cells[cell_itr]
+        if cell.get('cell_type') != 'code':
+            continue
+
+        codes = cell["source"]
+        for i in range(len(codes)):
+            for exp in exps:
+                if exp in codes[i]:
+                    codes[i] = ''
+
+        cell_ast = parse_ast(cell)
+
+        for itr in range(len(cell_ast.body)):
+            ast_obj = cell_ast.body[itr]
+            if isinstance(ast_obj, ast.Assign) and \
+                    isinstance(ast_obj.value, ast.Call) and \
+                    isinstance(ast_obj.value.func, ast.Attribute) and \
+                    isinstance(ast_obj.value.func.value, ast.Name) and \
+                    ast_obj.value.func.value.id == 'Workspace':
+                if ast_obj.value.func.attr == 'get':
+                    if ast_obj.targets[0].id == 'ws':
+                        cell_ast.body[itr].body = [ws_get]
+                    elif ast_obj.targets[0].id == 'workspace':
+                        cell_ast.body[itr].body = [workspace_get]
+                elif ast_obj.value.func.attr == 'from_config':
+                    if ast_obj.targets[0].id == 'ws':
+                        cell_ast.body[itr].body = [ws_config]
+                    elif ast_obj.targets[0].id == 'workspace':
+                        cell_ast.body[itr].body = [workspace_config]
+                notebook_data['cells'][cell_itr]['source'] = astunparse.unparse(cell_ast)
 
 
 if __name__ == '__main__':
@@ -77,7 +123,7 @@ if __name__ == '__main__':
             notebook_data = json.load(nbfile)
 
         replace_kernel_to_notebook(notebook_data)
-        replace_workspace(notebook_data, path=folder)
+        replace_workspace(notebook_data)
 
         input_notebook = tempfile.mkstemp(suffix='.ipynb')[1]
         with open(input_notebook, 'w') as f:
