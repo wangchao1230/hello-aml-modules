3bdfbec2cb8c903d074dfb694ef6725c84802e37
liyw <liyw@microsoft.com>
2020-09-17 16:37:17 +0800

    update run_notebook.py

--- run_notebook.py
+++ run_notebook.py
@@ -15,10 +15,10 @@ exps = ["!az login -o none",
         "!az ml folder attach -w $WORKSPACE_NAME -g $RESOURCE_GROUP_NAME"]
 
 
-def run_notebook(notebook, output_folder):
+def run_notebook(notebook, output_folder, notebook_file_name):
     papermill.execute_notebook(
         input_path=notebook,
-        output_path=output_folder + "\\" + os.path.basename(notebook)[:-6] + "_output" + extension,
+        output_path=output_folder + "\\" + notebook_file_name[:-6] + "_output" + extension,
         log_output=True,
     )
 
@@ -29,11 +29,13 @@ def replace_kernel_to_notebook(notebook_data):
     :param notebook_data: the notebook data to set kernel
     :type notebook_data: dict
     """
-    notebook_data['metadata']['kernelspec'] = dict(
-        display_name="python3",
-        language="python",
-        name="python3"
-    )
+    if 'metadata' in notebook_data.keys():
+        if 'kernelspec' in notebook_data['metadata'].keys():
+            notebook_data['metadata']['kernelspec'] = dict(
+                display_name="python3",
+                language="python",
+                name="python3"
+            )
 
 
 def remove_exps(notebook_data):
@@ -54,11 +56,16 @@ def remove_exps(notebook_data):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--notebook_folder_directory', type=str, nargs='+', help='Notebook folder directory', required=True)
+    parser.add_argument('--notebooks_to_skip', type=str, nargs='+', help='Skipped notebooks')
     parser.add_argument('--fail_fast_flag', type=bool, help='Whether to stop running notebooks immediately when error occurs')
 
     args, _ = parser.parse_known_args(sys.argv)
 
     folder_list = args.notebook_folder_directory
+    notebooks_to_skip = args.notebooks_to_skip
+    if notebooks_to_skip is None:
+        print("notebooks_to_skip:", notebooks_to_skip)
+        notebooks_to_skip = []
     fail_fast_flag = args.fail_fast_flag
 
     summary = "\n\n=============================RUN SUMMARY=============================\n"
@@ -82,6 +89,11 @@ if __name__ == '__main__':
 
         notebooks = glob.glob(glob_path)
         for notebook in notebooks:
+            notebook_file_name = os.path.basename(notebook)
+            # Skip this notebook
+            if notebook_file_name in notebooks_to_skip:
+                continue
+
             with open(notebook) as nbfile:
                 notebook_data = json.load(nbfile)
 
@@ -94,11 +106,10 @@ if __name__ == '__main__':
 
             # Run Notebook
             try:
-                notebook_file_name = os.path.basename(notebook)
-                print("==============Running {}==============".format(notebook_file_name))
+                print("\n==============Running {}==============".format(notebook_file_name))
                 start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
                 start_time2 = time.time()
-                run_notebook(input_notebook, output_folder)
+                run_notebook(input_notebook, output_folder, notebook_file_name)
                 end_time2 = time.time()
                 end_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
                 summary += "[Success] Run notebook {0} elapsed: {1} seconds. Start time: {2}, End Time: {3}.\n".format(notebook_file_name, round(end_time2-start_time2, 2), start_time, end_time)
